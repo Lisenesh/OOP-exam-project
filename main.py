0413ac051c1260303be4da973f8f05cf3a6041c0
Lisa <167996755+Lisenesh@users.noreply.github.com>
2024-06-01 20:47:03 +0200

    Added new types of dresses, cleaned variables, added fancy stuff

--- main.py
+++ main.py
@@ -14,69 +14,70 @@ class Dress:
         return False
 
 
-
 class WeddingDress(Dress):
-    def __init__(self, name, color, length, material, price):
-        super().__init__(name, color, length, material, price)
+    def __init__(self, name, length, material, price):
+        super().__init__(name, "white", length, material, price)
         self.type = self.__class__.__name__
-        self.name = name
-        self.color = color
-        self.length = length
-        self.material = material
-        self.price = price
 
 
 class SummerDress(Dress):
-    def __init__(self, name, color, length, material, price):
-        super().__init__(name, color, length, material, price)
+    def __init__(self, name, color, length, price):
+        super().__init__(name, color, length, "cotton", price)
         self.type = self.__class__.__name__
-        self.name = name
-        self.color = color
-        self.length = length
-        self.material = material
-        self.price = price
 
 
 class EveningDress(Dress):
-    def __init__(self, name, color, length, material, price):
-        super().__init__(name, color, length, material, price)
+    def __init__(self, name, material, price):
+        super().__init__(name, "dark", "medium", material, price)
         self.type = self.__class__.__name__
-        self.name = name
-        self.color = color
-        self.length = length
-        self.material = material
-        self.price = price
 
 
 class BallGown(Dress):
-    def __init__(self, name, color, length, material, price):
-        super().__init__(name, color, length, material, price)
+    def __init__(self, name, color, material, price):
+        super().__init__(name, color, "long", material, price)
         self.type = self.__class__.__name__
-        self.name = name
-        self.color = color
-        self.length = length
-        self.material = material
-        self.price = price
 
 
 dresses = [
-    WeddingDress("Flondon", "white", "long", "silk", 100),
-    WeddingDress("Gondor", "white", "short", "cotton", 66),
-    WeddingDress("Liskom", "lila", "long", "cotton", 139),
-    SummerDress("Lalaba", "yellow", "medium", "silk", 55),
-    SummerDress("Trinda", "aqua", "short", "cotton", 150),
-    SummerDress("Skilora", "ruby", "medium", "cotton", 79)
+    WeddingDress("Flondon", "long", "silk", 120),
+    WeddingDress("Gondor", "short", "cotton", 176),
+    WeddingDress("Liskom",  "long", "cotton", 139),
+    SummerDress("Lalimba", "yellow", "medium", 55),
+    SummerDress("Trinda", "aqua", "short", 63),
+    SummerDress("Skilora", "ruby", "medium", 79),
+    EveningDress("Pirezi", "chiffon", 155),
+    EveningDress("Lamida", "silk", 159),
+    EveningDress("Verosiks", "velvet", 167),
+    BallGown("Vrisket", "lightblue", "wool", 55),
+    BallGown("Baret", "pink", "cotton", 150),
+    BallGown("Erida", "lila", "velvet", 79)
 ]
 
 # The main filter is the type of the dress
 # The filters after that will be affected by the type of dress chosen
-userTypedT = input("Input Dress type:")
-userTypedL = input("Input Dress length:")
-userTypedM = input("Input Dress material:")
-userTypedC = input("Input Dress color:")
+userTypedT = input("Input Dress type(WeddingDress/SummerDress/EveningDress/BallGown):")
+match userTypedT:
+    case "WeddingDress":
+        userTypedL = input("Input Dress length(long/short):")
+        userTypedM = input("Input Dress material(silk/cotton):")
+        userTypedC = "white"
+    case "SummerDress":
+        userTypedL = input("Input Dress length(medium/short):")
+        userTypedM = "cotton"
+        userTypedC = input("Input Dress color(yellow/aqua/ruby):")
+    case "EveningDress":
+        userTypedL = "medium"
+        userTypedM = input("Input Dress material(chiffon/silk/velvet):")
+        userTypedC = "dark"
+    case "BallGown":
+        userTypedL = "long"
+        userTypedM = input("Input Dress material(wool/cotton/velvet):")
+        userTypedC = input("Input Dress color(lightblue/pink/lila):")
 x = 0
 for dress in dresses:
     if not dress.findDressName(userTypedT, userTypedL, userTypedM, userTypedC):
         x += 1
         if x == len(dresses):
             print("Sorry, we have found no such dresses")
+
+print(userTypedT, userTypedL, userTypedM, userTypedC)
